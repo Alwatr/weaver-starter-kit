99fdd52021cb4f62006a8c642543acf7207b8fca
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-11-21 16:36:38 +0330

    feat(app): base 11ty structure

---
+++ packages/app/eleventy.config.js
@@ -0,0 +1,38 @@
+/**
+ * 11ty configuration.
+ * @param {import("@11ty/eleventy").UserConfig} eleventyConfig
+ * @returns {ReturnType<import("@11ty/eleventy/src/defaultConfig")>}
+ */
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({
+    assets: '/',
+  });
+
+  eleventyConfig.setQuietMode(true);
+
+  eleventyConfig.setServerOptions({
+    liveReload: true,
+    port: 8080,
+    showAllHosts: true,
+
+    /**
+     * Whether DOM diffing updates are applied where possible instead of page reloads
+     */
+    domDiff: false,
+  });
+
+  eleventyConfig.addWatchTarget('./site/');
+
+  return {
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    templateFormats: ['njk', '11ty.js', 'md'],
+    dir: {
+      input: 'site',
+      output: 'dist',
+      includes: '_includes',
+      data: '_data',
+      layouts: '_layouts',
+    },
+  };
+};
