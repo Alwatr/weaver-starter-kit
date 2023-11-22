0cf82cb8af84d21a29108eb01bb26ef25cf0afdf
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-11-22 15:12:20 +0330

    feat(app/build): add postcss before eleventy

--- packages/app/script/build.js
+++ packages/app/script/build.js
@@ -1,13 +1,16 @@
 import eleventy from '@11ty/eleventy';
 import {logger} from './logger.js';
+import {eleventyConfig} from './config.js';
+import {postcssBuild} from './postcss.js';
 
 async function build() {
   logger.logMethod?.('build');
-  const output = new eleventy('site', 'dist', {
-    configPath: './script/eleventy.config.cjs',
-  });
+  await postcssBuild();
+
+  const output = new eleventy('site', 'dist', {}, eleventyConfig);
   await output.write();
-  logger.logOther?.('build-success');
+
+  logger.logOther?.('build-success ✅');
 }
 
 build();
