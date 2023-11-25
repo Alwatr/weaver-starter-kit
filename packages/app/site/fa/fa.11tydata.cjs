c9de5b93be68d96ba6c10b1ffc2cedd337980f81
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-11-25 14:40:42 +0330

    feat(app): sample 11tydata file

--- packages/app/site/fa/fa.11tydata.cjs
+++ packages/app/site/fa/fa.11tydata.cjs
@@ -1,1 +1,17 @@
-module.exports = {};
+const eleventyFetch = require("@11ty/eleventy-fetch");
+
+module.exports = async function() {
+  try {
+    let json = await eleventyFetch("https://api.github.com/repos/Alwatr/pmpa", {duration: "1d",type: "json"});
+
+    return {
+      stargazers: json.stargazers_count
+    };
+  }
+  catch(e) {
+    console.log( "Failed getting GitHub stargazers count, returning 0" );
+    return {
+      stargazers: 0
+    };
+  }
+};
