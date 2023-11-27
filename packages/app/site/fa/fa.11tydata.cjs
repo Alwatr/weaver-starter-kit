60398d1a64d125a115fefe1b9a2f5151b936b688
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-11-27 05:04:22 +0330

    chore(app): remove 11ty fetch

--- packages/app/site/fa/fa.11tydata.cjs
+++ packages/app/site/fa/fa.11tydata.cjs
@@ -1,17 +1,5 @@
-const eleventyFetch = require('@11ty/eleventy-fetch');
-
 module.exports = async function () {
-  try {
-    let json = await eleventyFetch('https://api.github.com/repos/Alwatr/pmpa', {duration: '1d', type: 'json'});
-
-    return {
-      stargazers: json.stargazers_count,
-    };
-  }
-  catch (e) {
-    console.log('Failed getting GitHub stargazers count, returning 0');
-    return {
-      stargazers: 0,
-    };
+  return {
+    stargazers: 1,
   }
 };
