76460291cf75326cda94c7c9a7a885311756bd03
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-11-28 18:07:25 +0330

    chore(ci): cleanup

--- packages/api/script/logger.js
+++
@@ -1,7 +0,0 @@
-import {createLogger} from '@alwatr/logger';
-import {env} from 'process';
-
-export const logger = createLogger('pmpa-api', true);
-
-export const devMode = env.NODE_ENV !== 'production';
-logger.logProperty?.('devMode', devMode)
