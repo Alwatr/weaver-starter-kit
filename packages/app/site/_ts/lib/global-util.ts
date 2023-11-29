05c35b7d15f79acf26ddd15f05c104358c7c099c
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-11-29 16:36:07 +0330

    feat(app/global-util): add i18n

--- packages/app/site/_ts/lib/global-util.ts
+++ packages/app/site/_ts/lib/global-util.ts
@@ -1,13 +1,29 @@
+import {l10n} from '@alwatr/i18n';
+
 import {logger} from './logger.js';
 
 logger.logModule?.('global-util');
 
 declare global {
   interface Window {
+    l10n: typeof l10n;
     clearLocalStorage: typeof clearLocalStorage;
     devMode: typeof devMode;
   }
 }
+
+l10n.setResourceLoader((locale) => {
+  return {
+    ok: true,
+    meta: {
+      code: locale.code,
+      rev: 0,
+    },
+    data: {},
+  };
+});
+
+l10n.setLocale('auto');
 
 export function clearLocalStorage() {
   logger.logMethod?.('clearLocalStorage');
@@ -23,5 +39,6 @@ export function devMode() {
   location.reload();
 }
 
+window.l10n = l10n;
 window.clearLocalStorage = clearLocalStorage;
 window.devMode = devMode;
