731b13ad5ec5598f9396344af190ca043c8e0872
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2024-01-16 15:33:28 +0330

    chore(app): build logger name

--- packages/app/script/logger.mjs
+++ packages/app/script/logger.mjs
@@ -1,6 +1,6 @@
 import {definePackage} from '@alwatr/logger';
 
-export const logger = definePackage('pmpa-app/11ty', '1.x', true);
+export const logger = definePackage('pmpa-app-build', '1.x', true);
 
 export const devMode = process.env.NODE_ENV !== 'production';
 logger.logProperty?.('devMode', devMode);
