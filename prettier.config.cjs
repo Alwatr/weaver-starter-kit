5d72a106a7642b31795a6a2dc251289a306617fb
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2024-02-22 10:51:27 +0330

    feat: add liquid support

---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+const liquid = require.resolve('@shopify/prettier-plugin-liquid');
+const alwatrConfig = require('@alwatr/prettier-config');
+
+/** @type {import("prettier").Config} */
+const config = {
+  ...alwatrConfig,
+  plugins: [liquid]
+};
+
+module.exports = config;
