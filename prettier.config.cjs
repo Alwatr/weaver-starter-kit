4b111c3ed436f5be787a145929ea8d1d3c0022e5
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2024-03-02 13:30:15 +0330

    fix(app): prevent override alwatr prettier config plugins

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -4,7 +4,10 @@ const alwatrConfig = require('@alwatr/prettier-config');
 /** @type {import("prettier").Config} */
 const config = {
   ...alwatrConfig,
-  plugins: [liquid]
+  plugins: [
+    ...(alwatrConfig.plugins ?? []),
+    liquid
+  ],
 };
 
 module.exports = config;
