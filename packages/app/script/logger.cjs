15d58f7feec1b2b33e7a01aeb8d728b479d9f12c
Ali Mihandoost <ali.mihandoost@gmail.com>
2024-04-02 15:01:32 +0330

    feat(app/script): convert logger.cjs to logger.mjs

--- packages/app/script/logger.cjs
+++
@@ -1,8 +0,0 @@
-const {definePackage} = require('@alwatr/logger');
-
-const logger = definePackage('@alwatr/pmpa-app-build', '1.x', true);
-
-const devMode = process.env.NODE_ENV !== 'production';
-logger.logProperty?.('devMode', devMode);
-
-module.exports = {logger, devMode}
