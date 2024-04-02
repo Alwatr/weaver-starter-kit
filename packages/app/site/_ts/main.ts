064ff3ba5175b7bf6b8999cf3d2673f18080868c
Ali Mihandoost <ali.mihandoost@gmail.com>
2024-04-02 15:08:35 +0330

    feat(app/script): Refactor service worker registration and add event handlers

--- packages/app/site/_ts/main.ts
+++ packages/app/site/_ts/main.ts
@@ -3,6 +3,7 @@ import alpine from 'alpinejs';
 import {logger} from './lib/config.js';
 import './lib/global-util.js';
 import './lib/keep-scroll.js';
+// import './lib/service-worker.js';
 import './lib/type.js';
 
 logger.banner('@alwatr/pmpa');
