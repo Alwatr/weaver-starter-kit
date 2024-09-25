246e03499da9391901e880ac74aeb77ed7edcc64
Ali Mihandoost <ali.mihandoost@gmail.com>
2024-09-25 14:24:58 +0330

    refactor: new prerenderScript script

---
+++ packages/app/site/_include/prerender.data.cjs
@@ -0,0 +1,14 @@
+exports.render = function ({collections}) {
+  const prerenderUrls = collections.filter((page) => page.data.ignorePrerendering !== true).map((page) => page.url);
+
+  const prerenderScript = {
+    prerender: [
+      {
+        source: 'list',
+        urls: prerenderUrls,
+      },
+    ],
+  };
+
+  return `<script type="speculationrules">${JSON.stringify(prerenderScript)}</script>`;
+};
