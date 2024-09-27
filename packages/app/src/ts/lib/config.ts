69427a215478b6b089eee46285abf92995222de2
Ali Mihandoost <ali.mihandoost@gmail.com>
2024-09-27 23:16:21 +0330

    style: lint

--- packages/app/src/ts/lib/config.ts
+++ packages/app/src/ts/lib/config.ts
@@ -1,8 +1,8 @@
 import {localJsonStorage} from '@alwatr/local-storage';
 import {definePackage} from '@alwatr/logger';
 
-import type {} from '@alwatr/nano-build';
 import type {FetchOptions} from '@alwatr/flux';
+import type {} from '@alwatr/nano-build';
 
 export const logger = definePackage('@alwatr/weaver-app', __package_version__);
 logger.logModule?.('config');
