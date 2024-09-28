5cae969eee694824a9e3a96a0ce3c52a8c5d6b65
Mohammad Honarvar <honarvar.info@gmail.com>
2024-09-28 12:13:57 +0330

    fix(app): update configs

--- packages/app/builder/logger.mjs
+++ packages/app/builder/logger.mjs
@@ -1,6 +1,6 @@
 import {definePackage} from '@alwatr/logger';
 
-export const logger = definePackage('@alwatr/weaver-build', '5.x', true);
+export const logger = definePackage('@alwatr/weaver-build', __package_version__, true);
 
 export const devMode = process.env.NODE_ENV !== 'production';
 logger.logProperty?.('devMode', devMode);
