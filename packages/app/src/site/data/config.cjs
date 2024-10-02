efbab249e5091f03947b709cf3bbdea6aae472b2
Ali Mihandoost <ali.mihandoost@gmail.com>
2024-10-02 18:08:03 +0330

    feat(site/data): new configuration data

---
+++ packages/app/src/site/data/config.cjs
@@ -0,0 +1,29 @@
+const {env} = require('process');
+const {execSync} = require('child_process');
+
+function getLastCommitHash() {
+  return execSync('git rev-parse --short HEAD').toString().trim();
+}
+
+module.exports = {
+  siteUrl: env.siteUrl ?? '',
+
+  siteName: 'Sample Site Name',
+
+  theme: {
+    light: '#e2eaf8',
+    dark: '#084073',
+    splashBackground: '#0b0c16', // used in manifest
+  },
+  orientation: 'portrait', // used in manifest
+  defaultLocale: 'fa-IR', // used in manifest
+
+  googleAnalyticsKey: '',
+
+  currentChangeHash: getLastCommitHash(),
+
+  twitter: {
+    site: '@alidotmd',
+    creator: '@alidotmd',
+  },
+};
