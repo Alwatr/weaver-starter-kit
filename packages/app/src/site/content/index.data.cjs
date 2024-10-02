3010817f2f1cc8ba6ba71109476f46bfbff50e1a
Ali Mihandoost <ali.mihandoost@gmail.com>
2024-10-02 18:11:22 +0330

    refactor(layout): Remove deprecated site.json and add new index.data.cjs file

---
+++ packages/app/src/site/content/index.data.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  layout: 'base-body',
+  permalink: '{{ page.filePathStem }}.html',
+  title: 'Sample Page Title',
+  locale: 'fa-IR',
+  dir: 'rtl',
+  description: 'Sample Description',
+  socialImage: '/img/meta/social.jpeg',
+  // socialImageAlt: '',
+  tags: ['alwatr', 'weaver', 'alimd'],
+};
