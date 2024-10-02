8f3fd70a7640468c98491a17b47f561387855c7d
Ali Mihandoost <ali.mihandoost@gmail.com>
2024-10-02 13:45:26 +0330

    refactor(config): Refactor eleventy config, remove addPassthroughCopy, add addWatchTarget

--- packages/app/builder/config.mjs
+++ packages/app/builder/config.mjs
@@ -8,6 +8,7 @@ import directoryOutputPlugin from '@11ty/eleventy-plugin-directory-output';
 import {alwatrIcon} from './alwatr-icon.cjs';
 import EleventyRenderPlugin from '@11ty/eleventy/src/Plugins/RenderPlugin.js';
 import {generateServiceWorker} from './workbox.mjs';
+import { logger } from './logger.mjs';
 
 // https://github.com/11ty/eleventy/blob/v2.x/src/defaultConfig.js
 /**
@@ -18,10 +19,12 @@ import {generateServiceWorker} from './workbox.mjs';
 function eleventyConfig_(config) {
   let templateConfig = this;
 
-  config.addPassthroughCopy({
-    asset: '/', // copy all content/asset/* folder to root of dist.
-    'asset/img/meta/favicon.ico': '/favicon.ico',
-  });
+  // config.addPassthroughCopy({
+  //   asset: '/', // copy all content/asset/* folder to root of dist.
+  //   'asset/img/meta/favicon.ico': '/favicon.ico',
+  // }, {
+  //   debug: true
+  // });
 
   config.setServerOptions({
     liveReload: true,
@@ -34,7 +37,9 @@ function eleventyConfig_(config) {
     domDiff: false,
   });
 
-  // config.additionalWatchTargets = ['./content', './ts', './css'];
+  config.addWatchTarget('./src/css/');
+  config.addWatchTarget('./src/ts/');
+  logger.logProperty?.('additionalWatchTargets', config.additionalWatchTargets);
 
   // config.watchIgnores?.add('script');
 
@@ -85,7 +90,7 @@ function eleventyConfig_(config) {
       filesize: true,
       benchmark: true,
     },
-    warningFileSize: 400 * 1000,
+    warningFileSize: 400_000,
   });
 
   config.addTransform('minifyHtml', minifyHtml);
