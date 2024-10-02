4c0c5a0601499632833557dfe0d8cc08fb6dc89a
Ali Mihandoost <ali.mihandoost@gmail.com>
2024-10-02 18:11:47 +0330

    refactor(layout): Rename prerender.data.cjs to prerender.template.cjs and update exports
    - Renamed the file `prerender.data.cjs` to `prerender.template.cjs` in the `packages/app/src/site/include` directory.
    - Updated the exports from `exports.render` to `prerenderTemplate` function.

--- packages/app/src/site/include/prerender.template.cjs
+++ packages/app/src/site/include/prerender.template.cjs
@@ -1,6 +1,5 @@
-exports.render = function ({collections}) {
+function prerenderTemplate({collections}) {
   const prerenderUrls = collections.filter((page) => page.data.ignorePrerendering !== true).map((page) => page.url);
-
   const prerenderScript = {
     prerender: [
       {
@@ -9,6 +8,7 @@ exports.render = function ({collections}) {
       },
     ],
   };
-
   return `<script type="speculationrules">${JSON.stringify(prerenderScript)}</script>`;
-};
+}
+
+module.exports = prerenderTemplate;
