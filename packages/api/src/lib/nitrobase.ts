eb8d7de2b518c60bcbf1ac033214293f8b59150b
Ali Mihandoost <ali.mihandoost@gmail.com>
2024-10-08 09:34:27 +0330

    feat(api):: Add function to initialize missing stores in nitrobase

--- packages/api/src/lib/nitrobase.ts
+++ packages/api/src/lib/nitrobase.ts
@@ -3,3 +3,10 @@ import {AlwatrNitrobase} from 'alwatr/nitrobase';
 import {config} from './config.js';
 
 export const alwatrNitrobase = new AlwatrNitrobase(config.nitrobase);
+
+export function initializeStore() {
+  for (const store of Object.values(config.stores)) {
+    if (alwatrNitrobase.hasStore(store)) continue;
+    alwatrNitrobase.newCollection(store);
+  }
+}
