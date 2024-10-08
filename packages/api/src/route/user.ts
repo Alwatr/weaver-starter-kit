306bc760b35460c85da81e6f3cf67eece34f62d6
Ali Mihandoost <ali.mihandoost@gmail.com>
2024-10-08 12:21:57 +0330

    fix(api): Update user route to use DictionaryReq type

--- packages/api/src/route/user.ts
+++ packages/api/src/route/user.ts
@@ -4,7 +4,7 @@ import {alwatrNitrobase} from '../lib/nitrobase.js';
 import {nanotronApiServer} from '../lib/server.js';
 import {parseBodyAsJson} from '../pre-handler/parse-request-body.js';
 
-nanotronApiServer.defineRoute<{body: UserFormData}>({
+nanotronApiServer.defineRoute<{body: DictionaryReq}>({
   method: 'PUT',
   url: '/save-user',
   preHandlers: [parseBodyAsJson],
@@ -12,7 +12,7 @@ nanotronApiServer.defineRoute<{body: UserFormData}>({
     logger.logMethodArgs?.('defineRoute(`/save-user`)', {userData: this.sharedMeta.body});
 
     // add new user to the user's collection
-    const usersCollection = await alwatrNitrobase.openCollection<UserFormData>(config.stores.usersCollection);
+    const usersCollection = await alwatrNitrobase.openCollection<DictionaryReq>(config.stores.usersCollection);
 
     const userId = cryptoFactory.generateUserId();
     usersCollection.addItem(userId, this.sharedMeta.body);
